0ac0992e14728d7119c7a1a2c012ee7c0b3bc317
zohir <zohir@DESKTOP-GBMD9S2.lan1>
2020-04-10 23:36:29 -0400

    commit

--- QA_PRO_JAVA/src/zohirul_Alom/Conditional.java
+++ QA_PRO_JAVA/src/zohirul_Alom/Conditional.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 public class Conditional {
 	/*
 	 * Symbol
+	 * 
 	 *  ?   : 
+	 *  
 	 *  */
 
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		
-		int age1, age2, older;
+		int age1, age2, older,older1;
 		System.out.print("Enter age1 & age2: ");
 		age1= input.nextInt();
 		age2= input.nextInt();
 		
 		older = (age1>age2) ? age1 : age2;
+		older1=(age2<age1) ? age2: age1;
 		
 		System.out.println(older);
+		System.out.println(older1);
 		
 		
 
