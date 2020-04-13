4da46e5f3a68646cb17dfc46d0090b0c38b3ca43
zohir <zohir@DESKTOP-GBMD9S2.lan1>
2020-04-12 23:07:18 -0400

    commit

---
+++ QA_PRO_JAVA/src/zohirul_Alom/Variable_Type_nd_data.java
@@ -0,0 +1,28 @@
+package zohirul_Alom;
+
+public class Variable_Type_nd_data {
+	
+	//Variable declaration syntax......
+	//data_type variable_name;  
+
+	public static void main(String[] args) {
+		
+		boolean b = true;
+		char c = 'A';
+		short s = 27000;
+		int i = 35000;
+		float f = 10.3f;
+		double d = 120.2;
+		
+		
+		System.out.printf("boolean B = %b \n",b);
+		System.out.printf("character C = %c \n",c);
+		System.out.printf("short S = %s \n",s);
+		System.out.printf("I nteger I = %d \n",i);
+		System.out.printf("Float F = %.1f \n",f);
+		System.out.printf("Doublr D = %.3f \n",d);
+		
+
+	} 
+
+}
