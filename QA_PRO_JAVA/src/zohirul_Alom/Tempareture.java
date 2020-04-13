d759b01212e824f3e9c3f8a7ff1377965b587848
zohir <zohir@DESKTOP-GBMD9S2.lan1>
2020-04-13 04:26:13 -0400

    RESOLVED

--- QA_PRO_JAVA/src/zohirul_Alom/Tempareture.java
+++ QA_PRO_JAVA/src/zohirul_Alom/Tempareture.java
@@ -10,20 +10,21 @@ public class Tempareture {
 		
 		Scanner sc = new Scanner(System.in);
 		double celsius, farenheight;
-		
+		// Enter Celsisu = 15........
 		System.out.println("Enter celsius: ");
 		celsius=sc.nextDouble();
 		
-		farenheight = 1.8 * celsius + 32;
+		farenheight = 1.8 * (celsius + 32);
 		System.out.println("Farenheight: "+farenheight);
 		
 		System.out.println(" \n ");
 		
 		double faren, cels;
+		// Enter Farenheight = 70 ........
 		System.out.println("Enter Farenheight: ");
 		faren=sc.nextDouble();
 		
-		cels= 1.8 * faren -32;
+		cels= 0.55*(faren - 32);
 		System.out.println("Celsius: "+cels);
 		
 		
