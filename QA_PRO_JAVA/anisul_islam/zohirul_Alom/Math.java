f57452a49989d182c4145d263719ffa40e94e45f
zohir <zohir@192.168.1.99>
2020-04-17 14:19:59 -0400

    updated

---
+++ QA_PRO_JAVA/anisul_islam/zohirul_Alom/Math.java
@@ -0,0 +1,19 @@
+package zohirul_Alom;
+
+public class Math {
+
+	public static void main(String[] args) {
+		
+		int x = 25;
+		int y = 35;
+		
+		int max = java.lang.Math.max(x, y);
+		System.out.println("Maximum: "+max);
+		
+		int min = java.lang.Math.min(x, y);
+		System.out.println("Minimum: "+min);
+		
+
+	}
+
+}
