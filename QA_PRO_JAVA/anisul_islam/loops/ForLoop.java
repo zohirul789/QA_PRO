5a34d381486973d9f96c5d66f50f2839e8542bd9
zohir <zohir@192.168.1.99>
2020-04-17 14:42:41 -0400

    commit

---
+++ QA_PRO_JAVA/anisul_islam/loops/ForLoop.java
@@ -0,0 +1,14 @@
+package loops;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=15; i = i+2){
+			//System.out.println(" My Country Is Bangladesh ");
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ QA_PRO_JAVA/anisul_islam/loops/WhileLoop.java
@@ -0,0 +1,18 @@
+package loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i <= 15){
+			
+			System.out.println( i+" \nZOHIRUL ALOM \n10609 79th ST \nOZONE PARK, NY 11417 \n =============== ");
+			
+			i++;
+		}
+
+	}
+
+}
