768e264989e0c85f9d191325105a1c4f3181cc3a
zohir <zohir@192.168.1.99>
2020-04-17 14:49:23 -0400

    commit

---
+++ QA_PRO_JAVA/anisul_islam/loops/DoWhileLoop.java
@@ -0,0 +1,19 @@
+package loops;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		do{
+			
+			System.out.println( i+" \nZOHIRUL ALOM \n10609 79th ST \nOZONE PARK, NY 11417 \n =============== ");
+			
+			i++;
+			
+		}while(i<=50);
+
+	}
+
+}
