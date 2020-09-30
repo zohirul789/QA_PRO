be02a2c55fe02b127a6f3997eacb0bcb5ed98901
zohir <zohir@DESKTOP-GBMD9S2>
2020-09-30 04:53:42 -0400

    commit

---
+++ Java_Prectice/prectices/prectice/HomeWork_LoopsConcept.java
@@ -0,0 +1,21 @@
+package prectice;
+
+public class HomeWork_LoopsConcept {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1;i<=10;i++) {
+			System.out.println("\t");
+			
+			for(int j =1;j<=10;j++) {
+				System.out.print("\t");
+				System.out.print(i+"*"+j+"="+i*j+"");
+				
+				
+				
+			}
+		}
+
+	}
+
+}
---
+++ Java_Prectice/prectices/prectice/LoopsConcepts.java
@@ -0,0 +1,21 @@
+package prectice;
+
+public class LoopsConcepts {
+
+	public static void main(String[] args) {
+
+		String Debate = "WelCome To First Presidantsial Debates 2020";
+		String Candidate = "Trump vs Biden";
+		String Result = "Biden Win By 65%";
+	for(int i=0;i<=10;i++)	{
+		System.out.println(Debate);
+		System.out.println("*************************<>*******************");
+		System.out.println("\n");
+		System.out.println("\t"+Candidate);
+		System.out.println("\t"+Result);
+		System.out.println("\t");
+
+	}
+	}
+
+}
