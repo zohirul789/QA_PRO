5f67690dab8b7fc9f9b2a7b53c9abcb1e857a002
zohir <zohir@DESKTOP-GBMD9S2>
2020-10-09 17:33:23 -0400

    commit

---
+++ Java_Prectice/prectices/prectice/Loops_Concepts.java
@@ -0,0 +1,37 @@
+package prectice;
+
+public class Loops_Concepts {
+	
+	public static void main(String[] args) {
+		// for loops
+		System.out.println("*****For Loops*****"+"\n");
+		for(int i=1;i<=5;i++) {
+			System.out.println("Tournament Coming Soon");
+			
+		}
+		// while loops
+		System.out.println("\n");
+		System.out.println("*****While Loops*****"+"\n");
+		int j = 1;
+		while(j<=5) {
+			System.out.println("Philli Tournament on octuber 18");
+			j++;
+		}
+		// Do While Loops
+		System.out.println("\n");
+		System.out.println("*****Do While Loops*****"+"\n");
+		int k = 1;
+		do {
+			System.out.println("Next UpComing Tournament on Nov 2nd");
+			k++;
+		}
+		while(k <=6);{
+			
+			
+		}
+			
+		
+		
+	}
+
+}
